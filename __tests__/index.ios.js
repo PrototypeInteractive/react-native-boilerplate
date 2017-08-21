6d798ace1a897bd8f3b2d248adb428fcf32d192e
Khodor <khodor@prototype.net>
2017-08-21 18:53:33 +0400

    updated react-native-router-flux and fixed test issue with react-navigation

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import RNBoilerplate from '../app';
+import RNBoilerplate from '../app/index.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
